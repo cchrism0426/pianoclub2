9ad6feabaf6b8fbe25fd3a416a5a66b1ab75a1bc
cchrism0426 <cchrism0426@gmail.com>
2015-11-08 19:00:06 -0800

    more slide down nav bar

--- js/pianoClub.js
+++ js/pianoClub.js
@@ -1,21 +1,11 @@
 $(document).ready(function(){
 	var x = $(".dropDownFirst").width();
-	$(".dropDown").css("height", $(".dropDownFirst").height());
-    $(".dropDown").css("width", $(".dropDownFirst").width());
+	$(".element").css("height", $(".dropDownFirst").height());
+    $(".element").css("width", $(".dropDownFirst").width());
     $(".dropDownFirst").mouseenter(function(){
-        var submenu = $(this).parent().next();
-
-        submenu.css({
-        	position:'absolute',
-        	top: $(this).offset().top + $(this).height() + 'px',
-        	left: $(this).offset().left + 'px',
-        	z-index: 1000
-        });
-
-        submenu..slideDown();
-
-        submenu.mouseleave(function(){
-        $(".dropDown").slideUp();
-    });
-    });      
+        $("dropDown").slideDown();
+    });   
+    $(".menu").mouseleave(function(){
+        $("dropDown").slideUp();
+    });    
 });
