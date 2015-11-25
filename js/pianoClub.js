d6e31b33c958cfc71a143a28c561934bcce547fe
cchrism0426 <cchrism0426@gmail.com>
2015-11-24 16:46:46 -0800

    modified navbar
    removed jquery transitions and compacted the nav bar for index.html

--- js/pianoClub.js
+++ js/pianoClub.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
-	var a = $(".dropdown").width();
-	var b = $(".navbar").width();
-	var c = (b - 5*a)/2;
-	$(".button:first-of-type").css("margin-left", c);
+	// var a = $(".dropdown").width();
+	// var b = $(".navbar").width();
+	// var c = (b - 5*a)/2;
+	// $(".button:first-of-type").css("margin-left", c);
 	
-    $(".dropdown").mouseenter(function(){
-        $(".dropdownlist").slideDown();
-    });   
-    $(".dropdown").mouseleave(function(){
-        $(".dropdownlist").slideUp();
-    });    
+    var b = $(window).width();
+    $(".navbar").css("margin-left", .125*b);
+    $(".navbar").css("margin-right", .125*b);
+
+    // $(".dropdown").mouseover(function(){    
+    //     $(".dropdownlist").slideDown();
+    //     event.stopPropagation();
+    // });
+
+    // $(".dropdown").mouseleave(function(){
+    //     $(".dropdownlist").slideUp();
+    //     event.stopPropagation();
+    // }); 
 });
